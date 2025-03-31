5965297ef2945c6bd9c7cd3f4e91312a7e8251d1
Nick Bogatyrev <bogatyrev73@gmail.com>
2025-03-31 17:59:20 -0400

    Minor comments to mini and colorschemes lua files

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,3 +1,4 @@
+-- Loads in all of the colorschemes, you can still disable each one individually in their configs
 return {
   import = 'plugins.colorschemes',
 }
--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,3 +1,4 @@
+-- Import all of the mini plugins, they can still be individually disabled in their respective configs
 return {
   import = 'plugins.mini',
 }
