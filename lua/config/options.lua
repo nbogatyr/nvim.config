2ef2394a2cfc7c34adff3779ce4a2846a4346d0a
Nick Bogatyrev <bogatyrev73@gmail.com>
2025-04-03 19:00:12 -0400

    Added a bunch of LazyVim's default opts

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,28 +4,9 @@
 local opt = vim.opt
 local g = vim.g
 
--- Disable relative line numbers
-opt.relativenumber = false
-
--- Tabs ought to be 4 spaces
-opt.tabstop = 4
-opt.expandtab = true
-
--- Sets how neovim will display certain whitespace characters in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
-opt.list = true
-opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
-
--- Minimal number of screen lines to keep above and below the cursor.
-opt.scrolloff = 10
-
--- Disable wrapping by default
-opt.wrap = false
-
--- Set sessions opts
-opt.sessionoptions = "buffers,curdir,folds,globals,tabpages,winpos,winsize,help"
-
+-- ╭─────────────────────────────────────────────────────────╮
+-- │                         Globals                         │
+-- ╰─────────────────────────────────────────────────────────╯
 -- Taken from LazyVim defaults: https://www.lazyvim.org/configuration/general
 -- This file is automatically loaded by plugins.core
 g.mapleader = " "
@@ -35,9 +16,7 @@ g.maplocalleader = "\\"
 g.autoformat = true
 
 -- LazyVim completion engine to use.
--- Can be one of: nvim-cmp, blink.cmp
--- Leave it to "auto" to automatically use the completion engine
--- enabled with `:LazyExtras`
+-- Can be one of: nvim-cmp, blink.cmp, 'auto'
 g.lazyvim_cmp = "blink.cmp"
 
 -- if the completion engine supports the AI source,
@@ -51,12 +30,6 @@ g.ai_cmp = true
 -- * a function with signature `function(buf) -> string|string[]`
 g.root_spec = { "lsp", { ".git", "lua" }, "cwd" }
 
--- Optionally setup the terminal to use
--- This sets `vim.o.shell` and does some additional configuration for:
--- * pwsh
--- * powershell
--- LazyVim.terminal.setup("pwsh")
-
 -- Set LSP servers to be ignored when used with `util.root.detectors.lsp`
 -- for detecting the LSP root
 g.root_lsp_ignore = { "copilot" }
@@ -68,15 +41,47 @@ g.deprecation_warnings = false
 -- You can disable this for a buffer by setting `vim.b.trouble_lualine = false`
 g.trouble_lualine = true
 
-opt.autowrite = true -- Enable auto write
+-- Fix markdown indentation settings
+g.markdown_recommended_style = 0
+
+-- ╭─────────────────────────────────────────────────────────╮
+-- │                         Options                         │
+-- ╰─────────────────────────────────────────────────────────╯
+-- Disable relative line numbers
+opt.relativenumber = false
+
+-- Tabs ought to be 4 spaces
+opt.tabstop = 4
+
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+opt.list = true
+opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+-- Minimal number of screen lines to keep above and below the cursor.
+opt.scrolloff = 10
+
+-- Set sessions opts
+opt.sessionoptions = { "buffers", "curdir", "folds", "tabpages", "winsize", "help", "globals", "skiprtp", "folds" }
+
+-- Enable auto write
+opt.autowrite = true
+
 -- only set clipboard if not in ssh, to make sure the OSC 52
 -- integration works automatically. Requires Neovim >= 0.10.0
 opt.clipboard = vim.env.SSH_TTY and "" or "unnamedplus" -- Sync with system clipboard
 opt.completeopt = "menu,menuone,noselect"
-opt.conceallevel = 2 -- Hide * markup for bold and italic, but not markers with substitutions
-opt.confirm = true -- Confirm to save changes before exiting modified buffer
-opt.cursorline = true -- Enable highlighting of the current line
-opt.expandtab = true -- Use spaces instead of tabs
+
+-- Confirm to save changes before exiting modified buffer
+opt.confirm = true
+
+-- Enable highlighting of the current line
+opt.cursorline = true
+
+-- Use spaces instead of tabs
+opt.expandtab = true
+
+-- See ':h fillchars'
 opt.fillchars = {
   foldopen = "",
   foldclose = "",
@@ -87,7 +92,7 @@ opt.fillchars = {
 }
 opt.foldlevel = 99
 opt.formatexpr = "v:lua.require'lazyvim.util'.format.formatexpr()"
-opt.formatoptions = "jcroqlnt" -- tcqj
+opt.formatoptions = "jcqlnt" -- tcqj
 opt.grepformat = "%f:%l:%c:%m"
 opt.grepprg = "rg --vimgrep"
 opt.ignorecase = true -- Ignore case
@@ -95,15 +100,11 @@ opt.inccommand = "nosplit" -- preview incremental substitute
 opt.jumpoptions = "view"
 opt.laststatus = 3 -- global statusline
 opt.linebreak = true -- Wrap lines at convenient points
-opt.list = true -- Show some invisible characters (tabs...
 opt.mouse = "a" -- Enable mouse mode
 opt.number = true -- Print line number
 opt.pumblend = 10 -- Popup blend
 opt.pumheight = 10 -- Maximum number of entries in a popup
-opt.relativenumber = true -- Relative line numbers
 opt.ruler = false -- Disable the default ruler
-opt.scrolloff = 4 -- Lines of context
-opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp", "folds" }
 opt.shiftround = true -- Round indent
 opt.shiftwidth = 2 -- Size of an indent
 opt.shortmess:append({ W = true, I = true, c = true, C = true })
@@ -117,9 +118,8 @@ opt.splitbelow = true -- Put new windows below current
 opt.splitkeep = "screen"
 opt.splitright = true -- Put new windows right of current
 opt.statuscolumn = [[%!v:lua.require'snacks.statuscolumn'.get()]]
-opt.tabstop = 2 -- Number of spaces tabs count for
 opt.termguicolors = true -- True color support
-opt.timeoutlen = g.vscode and 1000 or 300 -- Lower than default (1000) to quickly trigger which-key
+opt.timeoutlen = 200 -- Lower than default (1000) to quickly trigger which-key
 opt.undofile = true
 opt.undolevels = 10000
 opt.updatetime = 200 -- Save swap file and trigger CursorHold
@@ -127,16 +127,3 @@ opt.virtualedit = "block" -- Allow cursor to move where there is no text in visu
 opt.wildmode = "longest:full,full" -- Command-line completion mode
 opt.winminwidth = 5 -- Minimum window width
 opt.wrap = false -- Disable line wrap
-
-if vim.fn.has("nvim-0.10") == 1 then
-  opt.smoothscroll = true
-  opt.foldexpr = "v:lua.require'lazyvim.util'.ui.foldexpr()"
-  opt.foldmethod = "expr"
-  opt.foldtext = ""
-else
-  opt.foldmethod = "indent"
-  opt.foldtext = "v:lua.require'lazyvim.util'.ui.foldtext()"
-end
-
--- Fix markdown indentation settings
-g.markdown_recommended_style = 0
