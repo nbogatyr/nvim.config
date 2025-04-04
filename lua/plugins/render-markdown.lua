7922b4c15a316142f9e17b8088c78e8d982f10da
Nick Bogatyrev <bogatyrev73@gmail.com>
2025-04-04 15:29:36 -0400

    Added copilot chat and rendermarkdown

---
+++ lua/plugins/render-markdown.lua
@@ -0,0 +1,13 @@
+return {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use the mini.nvim suite
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {
+    completions = {
+      blink = {
+        enabled = true
+      }
+    }
+  },
+}
