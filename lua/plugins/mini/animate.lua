329b10cf69699807e47de2960808e66743725b95
Nick Bogatyrev <bogatyrev73@gmail.com>
2025-04-04 00:56:43 -0400

    Removed mini-animate extra and put settings into my own file. Disabled cursor since I use kitty cursor anims

--- lua/plugins/mini/animate.lua
+++ lua/plugins/mini/animate.lua
@@ -1,10 +1,23 @@
 return {
 
   "echasnovski/mini.animate",
+  event = "VeryLazy",
   enabled = true,
 
   opts = function(_, opts)
-    opts.scroll.enable = false
+    -- Disable scrolling
+    opts.scroll = { enable = false }
+    opts.cursor = { enable = false }
+
+    -- Disable mini.animate in grug-far filetype
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "grug-far",
+      callback = function()
+        vim.b.minianimate_disable = true
+      end,
+    })
+
+    -- Add a toggle for mini animations
     Snacks.toggle({
       name = "Mini Animate",
       get = function()
@@ -14,5 +27,7 @@ return {
         vim.g.minianimate_disable = not state
       end,
     }):map("<leader>uB")
+
+    return opts
   end,
 }
