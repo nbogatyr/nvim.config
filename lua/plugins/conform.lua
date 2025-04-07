5a155cce5fca6066d271f548661f51efc98f590f
Nick Bogatyrev <bogatyrev73@gmail.com>
2025-04-06 21:42:20 -0400

    Replaced isort and black with ruff and added prettier and markdown toc

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -6,8 +6,10 @@ return {
   opts = {
     notify_on_error = true,
     notify_no_formatters = true,
+
     formatters_by_ft = {
-      python = { "isort", "black" },
+      python = { "ruff_fix", "ruff_organize_imports", "ruff_format" },
+      markdown = { "prettier", "markdown-toc" },
     },
   },
 }
