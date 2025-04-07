85cf9ffe5aeeebd858cb429f1e8d55faf2d7b3cb
Nick Bogatyrev <bogatyrev73@gmail.com>
2025-04-06 21:41:06 -0400

    New autocmds for markdown-toc and CsvView toggles

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -47,3 +47,33 @@ vim.api.nvim_create_autocmd("ModeChanged", {
     pcall(vim.diagnostic.show)
   end,
 })
+
+-- Add keymap for markdown-toc to create a toc at cursor only in markdown files
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("formatters_markdown", {}),
+  pattern = { "md", "markdown" },
+  callback = function()
+    vim.schedule(function()
+      vim.keymap.set("n", "<leader>czt", function()
+        require("config.extras").insert_at_start_of_line("<!-- toc -->")
+        require("conform").format({ timeout_ms = 500 })
+      end, { desc = "Creates a table of contents for the current markdown buffer using markdown-toc" })
+    end)
+  end,
+})
+
+-- Add keymap for CsvView keymaps so that they only trigger in CSV files
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("formatters_csv", {}),
+  pattern = { "csv" },
+  callback = function()
+    vim.schedule(function()
+      vim.keymap.set(
+        "n",
+        "<leader>cvt",
+        vim.cmd["CsvViewToggle"],
+        { desc = "Creates a table of contents for the current markdown buffer using markdown-toc" }
+      )
+    end)
+  end,
+})
