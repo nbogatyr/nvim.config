061cda66199999276b0aeca6629bcb6a92a45d3d
Nick Bogatyrev <bogatyrev73@gmail.com>
2025-04-06 21:43:09 -0400

    Added nvim-lint and configured it for markdown for now

---
+++ lua/plugins/nvim-lint.lua
@@ -0,0 +1,19 @@
+return {
+  "mfussenegger/nvim-lint",
+  event = "LazyFile",
+
+  opt = {
+    -- Events to trigger linters
+    events = {
+      "BufWritePost",
+      "BufReadPost",
+      "InsertLeave",
+    },
+
+    linters_by_ft = {
+      markdown = { "markdownlint" },
+    },
+
+    linters = {},
+  },
+}
