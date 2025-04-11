c9025b6a6d9c54a3d794c35be9d976f2ecea3121
Nick Bogatyrev <bogatyrev73@gmail.com>
2025-04-11 16:32:56 -0400

    Adjusted blink keybinds, changed blink auto complete layout

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -4,5 +4,23 @@ return {
 
     -- Always use rust implementation
     fuzzy = { implementation = "rust" },
+    completion = {
+      menu = {
+        border = nil,
+
+        draw = {
+          columns = { { "kind_icon", "label", gap = 1 }, { "source_name", "label_description", gap = 1 } },
+        },
+      },
+    },
+
+    keymap = {
+      ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+      ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
+      ["<PageUp>"] = { "scroll_documentation_up", "fallback" },
+      ["<PageDown>"] = { "scroll_documentation_down", "fallback" },
+      ["<C-d>"] = { "scroll_documentation_down", "fallback" },
+      ["<C-u>"] = { "scroll_documentation_up", "fallback" },
+    },
   },
 }
